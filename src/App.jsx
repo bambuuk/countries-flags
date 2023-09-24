caf7f16067973df80f6a427d61d907fa00851ad8
Ivan <vanyok.korobka@ukr.net>
2023-09-24 12:57:40 +0300

    feat/create MainContent block, Contrlos block with Search and Select elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Controls from "./components/Controls";
 import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <>
       <Header />
+      <MainContent>
+        <Controls />
+      </MainContent>
     </>
   );
 }
