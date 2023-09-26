bb9d5f432eac0ed584b3e89c5025f8487c8f264c
Ivan <vanyok.korobka@ukr.net>
2023-09-26 11:46:59 +0300

    feat/add basename to vite config, to routing file, add deploy file to worklows

--- src/services/routing.jsx
+++ src/services/routing.jsx
@@ -5,20 +5,25 @@ import NotFound from "../pages/NotFound";
 import HomePage from "../pages/HomePage";
 import Details from "../pages/Details";
 
-export const routing = createBrowserRouter([
+export const routing = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "/country/:name",
+          element: <Details />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "/country/:name",
-        element: <Details />,
-      },
-    ],
-  },
-]);
+    basename: "/countries-flags",
+  }
+);
